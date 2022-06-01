f52ed26132ad2dee4d418e430bbc3b761b5bb8e6
chocoboabby <93240698+chocoboabby@users.noreply.github.com>
2022-06-01 10:00:17 +0800

    initial setup of dictionary app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <a
+        className="App-link"
+        href="/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Open source code
+      </a>{" "}
+      <span>by Abby Dela Fuente</span>
+    </div>
+  );
+}
+
+export default App;
