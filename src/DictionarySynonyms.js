6e55d84fa1d502a68bd8dc95a3c4cd7deaa8d90d
chocoboabby <93240698+chocoboabby@users.noreply.github.com>
2022-06-07 20:29:44 +0800

    add container

--- src/DictionarySynonyms.js
+++ src/DictionarySynonyms.js
@@ -4,11 +4,13 @@ export default function DictionarySynonyms(props) {
   if (props.synonyms) {
     return (
       <div className="DictionarySynonyms">
-        <ul>
-          {props.synonyms.map(function (synonym, index) {
-            return <li key={index}>{synonym}</li>;
-          })}
-        </ul>
+        <div className="container">
+          <ul>
+            {props.synonyms.map(function (synonym, index) {
+              return <li key={index}>{synonym}</li>;
+            })}
+          </ul>
+        </div>
       </div>
     );
   } else {
